0c44438ab43ead10a8bbaa123d0f84a3d78dd478
Evening <evening@mstars-lab3-07s-iMac.local>
2023-04-05 19:35:54 +0800

    movies

---
+++ src/pages/movies/[movie].tsx
@@ -0,0 +1,13 @@
+import { useRouter } from "next/router";
+import React from "react";
+
+const Movie = () => {
+  const {} = useRouter();
+  return (
+    <div>
+      <h1>hi</h1>
+    </div>
+  );
+};
+
+export default Movie;
